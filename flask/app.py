fb0801f665f15fb755594b1634ef911af52b1ef5
maro5397 <lsj000424@gmail.com>
2022-02-18 20:17:45 +0900

    ⚡️ build: update files

---
+++ flask/app.py
@@ -0,0 +1,18 @@
+# pip install flask
+from flask import Flask, render_template
+from werkzeug.middleware.proxy_fix import ProxyFix
+import os
+
+app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
+
+color = os.environ.get('APP_COLOR')
+
+
+@app.route('/')
+def main():
+    return render_template('hello.html', color=color)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
